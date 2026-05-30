5fa86f6659cc210abdc62c23fcf52a1d6026b921
Alex Fridlyand <alex@fridlyand.com>
2026-05-30 16:51:43 -0700

    Blend macOS title bar into frame (transparent titlebar, light appearance)

--- early-init.el
+++ early-init.el
@@ -3,3 +3,5 @@
 
 (push '(font . "PragmataPro VF Mono-16") default-frame-alist)
 (setq ns-use-proxy-icon nil)
+(push '(ns-transparent-titlebar . t) default-frame-alist)
+(push '(ns-appearance . light) default-frame-alist)
