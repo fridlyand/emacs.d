c42d3c631c9391b755606e48345fe7b0dfc277cb
Alex Fridlyand <alex@fridlyand.com>
2026-05-30 22:44:47 -0700

    Housekeeping: redirect backups/autosaves out of source trees, no lockfiles, disposable custom-file

--- init.el
+++ init.el
@@ -1,6 +1,11 @@
 (setq inhibit-startup-screen t)
 (setq initial-scratch-message nil)
 
+(setq backup-directory-alist `(("." . ,(locate-user-emacs-file "backups")))
+      auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
+      create-lockfiles nil
+      custom-file (make-temp-file "emacs-custom-"))
+
 (setq font-lock-maximum-decoration 1)
 (show-paren-mode -1)
 (setq blink-matching-paren nil)
