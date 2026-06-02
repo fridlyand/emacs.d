7b5ad70b7333eff1b14980f5d7b1c17b1af313c7
Alex Fridlyand <alex@fridlyand.com>
2026-06-02 15:40:13 -0700

    Clean up: drop dark/plain themes + cycle + number regexp; regroup init/early-init; discard custom-file via null-device

--- init.el
+++ init.el
@@ -1,33 +1,18 @@
-(setq inhibit-startup-screen t)
-(setq initial-scratch-message nil)
+(setq inhibit-startup-screen t
+      initial-scratch-message nil)
 
 (setq backup-directory-alist `(("." . ,(locate-user-emacs-file "backups")))
       auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
       create-lockfiles nil
-      custom-file (make-temp-file "emacs-custom-"))
+      custom-file null-device)
 (save-place-mode 1)
 (desktop-save-mode 1)
 
 (setq font-lock-maximum-decoration 1)
-(add-hook 'prog-mode-hook
-          (lambda ()
-            (font-lock-add-keywords nil
-              '(("\\_<[[:digit:]][[:alnum:]._']*\\_>" (0 'font-lock-number-face))))))
-
 (show-paren-mode -1)
 (setq blink-matching-paren nil)
 
-(setq ns-command-modifier 'meta)
-(setq ns-option-modifier 'super)
+(setq ns-command-modifier 'meta
+      ns-option-modifier 'super)
 
 (load-theme 'naysayer-cream t)
-
-(defvar naysayer-theme-ring '(naysayer-cream naysayer-dark naysayer-plain))
-(defun naysayer-cycle-theme ()
-  (interactive)
-  (let* ((cur (seq-find (lambda (x) (memq x custom-enabled-themes)) naysayer-theme-ring))
-         (next (or (cadr (memq cur naysayer-theme-ring)) (car naysayer-theme-ring))))
-    (mapc #'disable-theme custom-enabled-themes)
-    (load-theme next t)
-    (message "%s" next)))
-(global-set-key (kbd "C-c t") #'naysayer-cycle-theme)
