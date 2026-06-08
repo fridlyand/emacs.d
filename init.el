2edd597174e36c41c3d4dd2e8e35dbdd6f6ee8e8
Alex Fridlyand <alex@fridlyand.com>
2026-06-08 09:01:47 -0700

    Simplify frame and font setup
    Single 11.5pt PragmataPro font in early-init (was per-device 12/16 set in two places). Drop frame maximization and the invisible-frame + make-frame-visible reveal (white flash is handled by the Emacs.* registry resource). Drop ns-appearance override; use setq default-directory instead of a startup hook for cd ~.

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
       custom-file null-device)
 (save-place-mode 1)
 
-(add-hook 'emacs-startup-hook (lambda () (cd "~")))
+(setq default-directory "~/")
 
 (setq font-lock-maximum-decoration 1)
 (setq c-default-style "linux")
@@ -16,17 +16,5 @@
 
 (setq ns-command-modifier 'meta
       ns-option-modifier 'super)
-
-(set-frame-font (if (< (display-mm-width) 400)
-                    "PragmataPro VF Mono-12"
-                  "PragmataPro VF Mono-16")
-                t t)
-
-(when (eq system-type 'windows-nt)
-  (add-hook 'window-setup-hook
-            (lambda ()
-              (set-frame-parameter nil 'fullscreen nil)
-              (set-frame-parameter nil 'fullscreen 'maximized)
-              (make-frame-visible))))
 
 (load-theme 'my t)
